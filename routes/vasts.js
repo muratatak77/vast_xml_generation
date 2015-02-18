52723a2e246de09ca7f8fdc0f0779acf7b086325
Murat Atak <murat@gliza.com>
2015-02-18 13:12:19 +0200

    added cdata

--- routes/vasts.js
+++ routes/vasts.js
@@ -17,18 +17,10 @@ router.get('/', function(req, res, next) {
 
 router.get('/vast3_xml_generation', function(req, res, next){
 
-	// vast3_xml.node('VAST').attr({version:'3.0' , 'xmlns:xsi':'http://www.w3.org/2001/XMLSchema-instance' , 'xsi:noNamespaceSchemaLocation':'vast.xsd'});
-  	// vast3_xml.node('root')
-   //  	.node('child').attr({foo: 'bar'})
-   //    		.node('grandchild', 'grandchild content').attr({baz: 'fizbuzz'})
-   //  	.parent()
-  	// .parent()
-   //  	.node('sibling', 'with content!');
 
-	vast3_xml = new libxmljs.Document();
-
+	vast3_xml = new libxmljs.Document('1.0','utf8');
 	temp_vast_xml = vast3_xml.node('VAST').attr({version:'3.0' , 'xmlns:xsi':'http://www.w3.org/2001/XMLSchema-instance' , 'xsi:noNamespaceSchemaLocation':'vast.xsd'});
-
+	
 	XMLPaths.forEach(function(value, index){
 		console.log("vast 2 xml file path : " , value);
 		xml_parse_build(value , index);
@@ -37,7 +29,6 @@ router.get('/vast3_xml_generation', function(req, res, next){
 	temp_vast_xml = temp_vast_xml.parent(); //Ad
 	vast3_xml = temp_vast_xml.toString(); 
 	console.log("vast 3.0 xml : " , vast3_xml.toString());
-
 
 	console.log("");
 	console.log("");
@@ -52,8 +43,8 @@ function xml_parse_build(XMLPath , index){
 
 	// console.log("index >> " , index);
 
-	var xmlFile = fs.readFileSync(XMLPath, 'utf-8');
-	var xmlDoc = libxmljs.parseXml(xmlFile);
+	var xmlFile = fs.readFileSync(XMLPath, 'UTF-8');
+	var xmlDoc = libxmljs.parseXmlString(xmlFile, { noblanks: true });
 
 	var vast = xmlDoc.get('//VAST');
 	var version = vast.attr('version').value();
@@ -83,9 +74,7 @@ function xml_parse_build(XMLPath , index){
 										VideoClicks(); //VideoClicks
 											ClickThrough(xmlDoc); //ClickThrough
 										temp_vast_xml = temp_vast_xml.parent(); //VideoClicks
-										MediaFiles(); //MediaFiles
-											MediaFile(xmlDoc); //MediaFile
-										temp_vast_xml = temp_vast_xml.parent(); //MediaFiles
+										MediaFiles(xmlDoc); //MediaFiles
 									temp_vast_xml = temp_vast_xml.parent(); //Linear
 								temp_vast_xml = temp_vast_xml.parent(); //Creative
 							temp_vast_xml = temp_vast_xml.parent(); //Creatives
@@ -97,27 +86,31 @@ function AdSystem(xmlDoc){
 	var AdSystem = xmlDoc.get('//AdSystem');
 	var AdSystem_version = AdSystem.attr('version').value();
 	temp_vast_xml = temp_vast_xml.node('AdSystem','FreeWheel').attr({version : AdSystem_version});
+
 	temp_vast_xml = temp_vast_xml.parent();
 	console.log("AdSystem version - FreeWheel : " , AdSystem_version);
 }
 
 function AdTitle(xmlDoc){
 	var AdTitle = xmlDoc.get('//AdTitle').text();
-	temp_vast_xml = temp_vast_xml.node('AdTitle', AdTitle );
+	temp_vast_xml = temp_vast_xml.node('AdTitle');
+	temp_vast_xml = temp_vast_xml.cdata(AdTitle);
 	temp_vast_xml = temp_vast_xml.parent();
 	console.log("AdTitle : " , AdTitle);
 }
 
 function Description(xmlDoc){
 	var Description = xmlDoc.get('//Description').text();
-	temp_vast_xml = temp_vast_xml.node('Description', Description );
+	temp_vast_xml = temp_vast_xml.node('Description');
+	temp_vast_xml = temp_vast_xml.cdata(Description);
 	temp_vast_xml = temp_vast_xml.parent();
 	console.log("Description : " , Description);
 }
 
 function Error(xmlDoc){
 	var Error = xmlDoc.get('//Error').text();
-	temp_vast_xml = temp_vast_xml.node('Error', Error);
+	temp_vast_xml = temp_vast_xml.node('Error');
+	temp_vast_xml = temp_vast_xml.cdata(Error);
 	temp_vast_xml = temp_vast_xml.parent();
 	console.log("Error : " , Error);
 } 							
@@ -127,7 +120,8 @@ function Impression(xmlDoc){
 	for (var i=0; i<ImpressionArr.length; i++){
 		var ImpressionArr_id = ImpressionArr[i].attr('id').value();
 		var ImpressionArr_text = ImpressionArr[i].text();
-		temp_vast_xml = temp_vast_xml.node('Impression' , ImpressionArr_text ).attr({'id' : ImpressionArr_id });
+		temp_vast_xml = temp_vast_xml.node('Impression').attr({'id' : ImpressionArr_id });
+		temp_vast_xml = temp_vast_xml.cdata(ImpressionArr_text);
 		temp_vast_xml = temp_vast_xml.parent();
 		console.log("Impression Id : " , ImpressionArr_id +" / text : "+ ImpressionArr_text );
 	}
@@ -164,7 +158,8 @@ function TrackingEvents(xmlDoc){
 			if (TrackingEventsArr[i].name() == "Tracking"){
 				var TrackingEvents_event = TrackingEventsArr[i].attr('event').value();
 		   		var TrackingEvents_text = TrackingEventsArr[i].text();
-		   		temp_vast_xml = temp_vast_xml.node('Tracking' , TrackingEvents_text ).attr({'event' : TrackingEvents_event });
+		   		temp_vast_xml = temp_vast_xml.node('Tracking').attr({'event' : TrackingEvents_event });
+		   		temp_vast_xml = temp_vast_xml.cdata(TrackingEvents_text);
 		   		temp_vast_xml = temp_vast_xml.parent();
 				console.log("TrackingEvents event : " , TrackingEvents_event +" / text : " + TrackingEvents_text );
 			}
@@ -179,20 +174,31 @@ function VideoClicks(){
 
 function ClickThrough(xmlDoc){
 	var ClickThrough = xmlDoc.get('//ClickThrough').text();
-	temp_vast_xml = temp_vast_xml.node('ClickTracking',ClickThrough);
+	temp_vast_xml = temp_vast_xml.node('ClickTracking');
+	temp_vast_xml = temp_vast_xml.cdata(ClickThrough);
 	temp_vast_xml = temp_vast_xml.parent();
 	console.log("ClickThrough  : " , ClickThrough);
 }
 
-function  MediaFiles(){
+function MediaFiles(xmlDoc){
+	var MediaFilesArr = xmlDoc.get('//MediaFiles').childNodes();
+	if (MediaFilesArr == null){
+		return 
+	}
 	temp_vast_xml = temp_vast_xml.node('MediaFiles'); //MediaFiles
+	console.log("MediaFilesArr size : " , MediaFilesArr.length );
+		for (var i=0; i<MediaFilesArr.length; i++ ){
+			if (MediaFilesArr[i].name() == "MediaFile"){
+				MediaFile(MediaFilesArr[i]);
+			}
+		}
+	temp_vast_xml = temp_vast_xml.parent(); //MediaFiles
 }
 
 
+function MediaFile(MediaFile){
 
-function MediaFile(xmlDoc){
-
-	var MediaFile = xmlDoc.get('//MediaFile');
+	// var MediaFile = xmlDoc.get('//MediaFile');
 	var MediaFile_delivery = MediaFile.attr('delivery').value();
 	var MediaFile_bitrate = MediaFile.attr('bitrate').value();
 	var MediaFile_width = MediaFile.attr('width').value();
@@ -206,45 +212,49 @@ function MediaFile(xmlDoc){
 				+" / type : " + MediaFile_type 
 				+" / file : " + MediaFile_text 
 				);
-	temp_vast_xml = temp_vast_xml.node('MediaFile' , MediaFile_text ).attr({
+	temp_vast_xml = temp_vast_xml.node('MediaFile').attr({
 										'delivery' : MediaFile_delivery , 
 										'bitrate':  MediaFile_bitrate ,
 										'width' : MediaFile_width ,   
 										'height' : MediaFile_height ,  
 										'type' : MediaFile_type
 										});
-	temp_vast_xml = temp_vast_xml.parent(); //MediaFile
+	
+	temp_vast_xml = temp_vast_xml.cdata(MediaFile_text);
+
+
+	return temp_vast_xml = temp_vast_xml.parent(); //MediaFile
 }
 
-	
 
-	
+
+
 
 
 
 router.get('/vast_test', function(req, res, next){
 
-	var xml =  '<?xml version="1.0" encoding="UTF-8"?>' +
-           '<root>' +
-               '<child foo="bar">' +
-                   '<grandchild baz="fizbuzz">grandchild content</grandchild>' +
-               '</child>' +
-               '<sibling>with content!</sibling>' +
-           '</root>';
+	// var xml =  '<?xml version="1.0" encoding="UTF-8"?>' +
+ //           '<root>' +
+ //               '<child foo="bar">' +
+ //                   '<grandchild baz="fizbuzz">grandchild content</grandchild>' +
+ //               '</child>' +
+ //               '<sibling>with content!</sibling>' +
+ //           '</root>';
  
-	var xmlDoc = libxmljs.parseXml(xml);
+	// var xmlDoc = libxmljs.parseXml(xml);
 	 
-	// xpath queries 
-	var gchild = xmlDoc.get('//grandchild');
+	// // xpath queries 
+	// var gchild = xmlDoc.get('//grandchild');
 	 
-	//console.log(gchild.text());  // prints "grandchild content" 
+	// //console.log(gchild.text());  // prints "grandchild content" 
 	 
-	var children = xmlDoc.root().childNodes();
-	//console.log("children >>>" , children); // prints "bar" 
+	// var children = xmlDoc.root().childNodes();
+	// //console.log("children >>>" , children); // prints "bar" 
 	
-	var child = children[0];
+	// var child = children[0];
 	 
-	//console.log(child.attr('foo').value()); // prints "bar" 
+	// //console.log(child.attr('foo').value()); // prints "bar" 
 
 	res.send('respond with a vast');    // echo the result back
 
